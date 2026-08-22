8326dde2d2c5477938a95280f11deb18f3293ea7
mugeswarangokulraj <mugeswaran601@gmail.com>
2026-08-22 09:24:56 +0530

    PGM-3 RDBMS 1U25IT059
    Formatted SQL code for better readability and consistency.

--- solution.sql
+++ solution.sql
@@ -2,14 +2,15 @@ CREATE DATABASE CollegeDB;
 USE CollegeDB;
 CREATE TABLE Student (
     StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20),
+    StudentName VARCHAR (20),
     DOB DATE ,
-    Gender VARCHAR(10) ,
-    DepartmentID INT(5) 
+    Gender VARCHAR (10) ,
+    DepartmentID INT (5) 
 );
 
 ALTER TABLE Student
-  ADD ( Email VARCHAR(30),
-  PHONENumber INT(10) );
+  ADD ( Email VARCHAR (30),
+        PHONENumber INT(10)
+    );
 
 DESC Student;
